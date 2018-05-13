4909a08c573464808c173ddc1a8dbb4b3cd21172
lciscon <lciscon@gmail.com>
2018-05-12 19:15:03 -0500

    jjj

--- octoprint_NWTools/__init__.py
+++ octoprint_NWTools/__init__.py
@@ -11,7 +11,7 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 import octoprint.printer
-import logging
+import octoprint.logging
 
 class NwtoolsPlugin(octoprint.plugin.SettingsPlugin,
                    octoprint.plugin.StartupPlugin,
@@ -65,16 +65,6 @@ class NwtoolsPlugin(octoprint.plugin.SettingsPlugin,
 		)
 
 def detect_machine_type(comm, line, *args, **kwargs):
-    if "MACHINE_TYPE" not in line:
-        return line
-
-    from octoprint.util.comm import parse_firmware_line
-
-    # Create a dict with all the keys/values returned by the M115 request
-    printer_data = parse_firmware_line(line)
-
-    logging.getLogger("octoprint.plugin." + __name__).info("Machine type detected: {machine}.".format(machine=printer_data["MACHINE_TYPE"]))
-
     return line
 
 
