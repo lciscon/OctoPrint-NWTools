1a0fa0cec143ca3be93c9ce6bdf76fa37e6b64d7
lciscon <lciscon@gmail.com>
2018-05-19 21:56:49 -0500

    ewe

--- octoprint_NWTools/__init__.py
+++ octoprint_NWTools/__init__.py
@@ -96,6 +96,6 @@ def __plugin_load__():
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
-#		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
 		"octoprint.comm.protocol.gcode.received": __plugin_implementation__.processGCODE
 	}
