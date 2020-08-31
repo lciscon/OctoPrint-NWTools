93852ac64f531163865db64d4637653b3f67a9d1
lciscon <lciscon@gmail.com>
2020-08-30 23:10:30 -0500

    Update NWTools.js

--- octoprint_NWTools/static/js/NWTools.js
+++ octoprint_NWTools/static/js/NWTools.js
@@ -214,19 +214,17 @@ $(function() {
 			self.showActionTriggerDialog(messageData, null);
 		} else if (data.action == "gridsaved") {
 			//the grid was saved.  run the fixgrid command and reopen the connection
-			console.log('Grid Saved... ');
-
-			self._postCommand("fixgrid", {});
-			self.reconnectSerial();
+//			self._postCommand("fixgrid", {});
+//			self.reconnectSerial();
 		} else if (data.action == "gridcomplete") {
 			//the grid scan is done.  save the grid.
-			console.log('Grid Complete... ');
-
 			sendPrinterCommand('M374'); //save the bed - triggers an action command that is used to fix the grid
 			sendPrinterCommand('M400');
 	        sendPrinterCommand('G0 Z2 F300');
 			sendPrinterCommand('M400');
 	  	  	sendPrinterCommand('M500'); //save changes
+			self._postCommand("fixgrid", {});
+			self.reconnectSerial();
 		} else if (data.action == "update") {
 			if (typeof data.tool0_ZOffset !== 'undefined') {
 				self.tool0_ZOffset["actual"](data.tool0_ZOffset);
